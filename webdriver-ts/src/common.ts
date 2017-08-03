d398beb5192369f9975e3a031fe9503e3c0c6c2a
Saurabh Nanda <saurabh@vacationlabs.com>
2017-08-03 13:05:35 +0530

    Changed the name of the benchmark

--- webdriver-ts/src/common.ts
+++ webdriver-ts/src/common.ts
@@ -63,6 +63,7 @@ export let frameworks = [
     f("marionette-v3.4.2-domapi-keyed", true),
     f("marionette-v3.4.2-keyed", true),
     f("marko-v4.3.0-keyed", true),
+    f("miso-non-official-v0.4.0.0-non-keyed", false),
     f("mithril-v1.1.1-keyed", true),
     f("moon-v0.11.0", false),
     f("nx-v1.0.0-beta.2.0.1-keyed", true),
@@ -101,5 +102,4 @@ export let frameworks = [
     f("vidom-v0.9.8-keyed", true),
     f("vue-v2.3.3-keyed", true),
     f("vue-v2.3.3-non-keyed", false),
-    f("miso-ghcjs-keyed", true),
 ]
