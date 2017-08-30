0040ae9d1dc732803c2b1c4c442ad449c81c3f7f
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2017-08-30 23:26:10 +0100

    Change GHCJS's GC interval from 1 second to 1 minute

--- reflex-dom-v0.4-keyed/build.sh
+++ reflex-dom-v0.4-keyed/build.sh
@@ -1,4 +1,4 @@
 cd reflex-platform/reflex-dom/reflex-dom
 ../../work-on ghcjs ./. --command "cabal configure --ghcjs --enable-benchmarks"
-../../work-on ghcjs ./. --command "cabal build --ghcjs-options='-dedupe -DGHCJS_BROWSER -O2 -fspecialise-aggressively'"
+../../work-on ghcjs ./. --command "cabal build --ghcjs-options='-dedupe -DGHCJS_BROWSER -O2 -fspecialise-aggressively -DGHCJS_GC_INTERVAL=60000'"
 cp -R dist/build/krausest/krausest.jsexe/* ../../../dist
